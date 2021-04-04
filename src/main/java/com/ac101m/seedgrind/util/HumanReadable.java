e4d3c880de2c37805beffb8a744b16721f561806
Alexander <alexander.cramb.94@gmail.com>
2021-04-04 17:14:27 +0100

    Add time formatting to HumanReadable

--- src/main/java/com/ac101m/seedgrind/util/HumanReadable.java
+++ src/main/java/com/ac101m/seedgrind/util/HumanReadable.java
@@ -1,12 +1,26 @@
 package com.ac101m.seedgrind.util;
 
 public class HumanReadable {
-    private static final String[] s_SIUnits = {"", "k", "M", "G", "T", "P", "E", "Z", "Y"};
+    private static final String[] s_suUnits = {"", "k", "M", "G", "T", "P", "E", "Z", "Y"};
+    private static final String[] s_tUnits = {"ns", "us", "ms", "s"};
 
-    public static String SI(double value) {
+    /**
+     * @param value Double containing dimensionless quantity.
+     * @return Human readable string representation.
+     */
+    public static String si(double value) {
+        return si(value, 3);
+    }
+
+    /**
+     * @param value Double containing dimensionless quantity.
+     * @param dp Number of decimal places to use.
+     * @return Human readable string representation.
+     */
+    public static String si(double value, int dp) {
         String units = "";
 
-        for (String s_siUnit : s_SIUnits) {
+        for (String s_siUnit : s_suUnits) {
             if (value < 1000) {
                 units = s_siUnit;
                 break;
@@ -14,6 +28,35 @@ public class HumanReadable {
             value /= 1000;
         }
 
-        return value + units;
+        return String.format("%." + dp + "f", value) + units;
+    }
+
+    /**
+     * @param time Double containing time in seconds.
+     * @return Human readable string representation of time (3 decimal places).
+     */
+    public static String time(double time) {
+        return time(time, 3);
+    }
+
+    /**
+     * @param time Double containing time in seconds.
+     * @param dp Decimal places to use.
+     * @return Human readable string representation of time.
+     */
+    public static String time(double time, int dp) {
+        String units = "";
+
+        time *= 1000000000.0;
+
+        for (String s_TUnits : s_tUnits) {
+            if (time < 1000) {
+                units = s_TUnits;
+                break;
+            }
+            time /= 1000;
+        }
+
+        return String.format("%." + dp + "f", time) + units;
     }
 }
