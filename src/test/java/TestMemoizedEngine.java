43485da51367927a45ca78ae9674c1315b459648
Alexander <alexander.cramb.94@gmail.com>
2021-04-04 17:17:51 +0100

    Add memoized engine speed test

--- src/test/java/TestMemoizedEngine.java
+++ src/test/java/TestMemoizedEngine.java
@@ -1,9 +1,8 @@
 import com.ac101m.seedgrind.biome.sampler.BiomeSampler;
+import com.ac101m.seedgrind.biome.search.Engine;
 import com.ac101m.seedgrind.biome.search.MemoizedEngine;
 import com.ac101m.seedgrind.biome.search.SimpleEngine;
-import com.ac101m.seedgrind.util.Area;
-import com.ac101m.seedgrind.util.SeedGrindException;
-import com.ac101m.seedgrind.util.Vec2;
+import com.ac101m.seedgrind.util.*;
 
 import kaptainwutax.biomeutils.Biome;
 import kaptainwutax.seedutils.mc.Dimension;
@@ -41,6 +40,35 @@ public class TestMemoizedEngine {
                     assert (foundBiomes.containsAll(expectedBiomes));
                 }
             }
+        }
+    }
+
+    @Test
+    public void testSpeed() throws SeedGrindException {
+        System.out.println("Resolution  Samples     duration    Samples/s   sqkm/s");
+
+        long seed = 0;
+
+        for (int resolution = 0; resolution < 10; resolution++) {
+            Area area = new Area(new Vec2(0, 0), 256 << resolution);
+            BiomeSampler biomeSampler = BiomeSampler.newSampler(MCVersion.v1_16_4, seed, Dimension.OVERWORLD);
+            Engine engine = new MemoizedEngine(biomeSampler, resolution, area);
+
+            long tStart = System.nanoTime();
+            engine.findAllBiomes(area);
+            long tEnd = System.nanoTime();
+
+            double durationSeconds = (double) (tEnd - tStart) / 1000000000.0;
+            double sampleRate = biomeSampler.getSampleCount() / durationSeconds;
+            double sqkmPerSample = (1 << resolution) * (1 << resolution) / 1000000.0;
+
+            System.out.println(
+                    StringUtil.padRight(String.valueOf(resolution), 12) +
+                    StringUtil.padRight(String.valueOf(biomeSampler.getSampleCount()), 12) +
+                    StringUtil.padRight(HumanReadable.time(durationSeconds), 12) +
+                    StringUtil.padRight(HumanReadable.si(sampleRate), 12) +
+                    StringUtil.padRight(HumanReadable.si(sqkmPerSample * sampleRate), 12)
+            );
         }
     }
 }
